2e39e3a0128994971360366ea91711001e90683f
ayensi <okanaltun9@gmail.com>
2023-03-22 15:21:24 +0300

    extras

--- ecommerce_b/product/urls.py
+++ ecommerce_b/product/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from product import views
 
 urlpatterns = [
-    path('latest-products', views.LatestProductLists.as_view())
+    path('latest-products/', views.LatestProductLists.as_view())
 ]
